c97b4416ea9541b52a2a6fb471ce8f2f892f627c
Mo. Fawwaaz <95509101+itsmfz@users.noreply.github.com>
2023-10-08 15:53:29 -0400

    Update main_test.py

--- src/main_test.py
+++ src/main_test.py
@@ -40,14 +40,7 @@ def read_key_from_file(filename='data/key.txt'):
         return None
     
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
-# ----------------------------------------------------------------------------
-def generate_random_key():
-    key = secrets.token_bytes(32)
-    return key
-
-# ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Print Key - This function is built to print the key 
 # ----------------------------------------------------------------------------
 def print_key(sk):
     if sk:
@@ -56,7 +49,7 @@ def print_key(sk):
         print(hex_values)
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Read plain text: This function is dedicated to only read the plaintext
 # ----------------------------------------------------------------------------
 def read_plain_text(filename='data/plaintext.txt'):
     try:
@@ -68,15 +61,15 @@ def read_plain_text(filename='data/plaintext.txt'):
         return None
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Encryption Block
 # ----------------------------------------------------------------------------
 def encrypt_block(block, key):
-    key_hash = sha256(key).digest()
-    encrypted_block = bytes(x ^ y for x, y in zip(block, key_hash))
+    key_hash = sha256(key).digest() #This line computes the SHA-256 hash of the encryption key (key) using Python's hashlib library. The .digest() method is called to obtain the binary representation of the hash.
+    encrypted_block = bytes(x ^ y for x, y in zip(block, key_hash)) # This line performs the encryption operation. It iterates over each byte in the block and XORs it with the corresponding byte from the key_hash. This creates the encrypted block.
     return encrypted_block
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Pad : padding to a block of binary data to ensure that its length is a multiple of a specified block size
 # ----------------------------------------------------------------------------
 def pad_data(data, block_size):
     padding_length = block_size - (len(data) % block_size)
@@ -84,7 +77,7 @@ def pad_data(data, block_size):
     return data + padding
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Hex_data to file: This writes the hex data to the file
 # ----------------------------------------------------------------------------
 def write_data_to_hex_file(data, filename):
     hex_data = b64encode(data).decode('utf-8')
@@ -92,7 +85,7 @@ def write_data_to_hex_file(data, filename):
         file.write(hex_data)
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Read Hex_data from file: This function is to read hex_data from file
 # ----------------------------------------------------------------------------
 def read_data_from_hex_file(filename):
     with open(filename, 'r') as file:
@@ -100,14 +93,14 @@ def read_data_from_hex_file(filename):
     return b64decode(hex_data)
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Unpad : for removing padding from a block of binary data
 # ----------------------------------------------------------------------------
 def unpad_data(data):
     padding_length = data[-1]
     return data[:-padding_length]
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Decrypt - This block is to decrypt and follows the same approach as the Encrypt block
 # ----------------------------------------------------------------------------
 def decrypt_block(block, key):
     key_hash = sha256(key).digest()
@@ -117,85 +110,118 @@ def decrypt_block(block, key):
 # ----------------------------------------------------------------------------
 # No code executed here; only module imports.
 # ----------------------------------------------------------------------------
-def save_decrypted_result(decrypted_data):
-    result_file = 'data/decrypted_result.txt'
-    with open(result_file, 'w') as file:
-        file.write(decrypted_data)
-
-# ----------------------------------------------------------------------------
-# No code executed here; only module imports.
-# ----------------------------------------------------------------------------
 def write_to_result(decrypted_plaintext):
     result_file= 'data/result.txt'
     with open(result_file, 'w') as file:
         file.write(decrypted_plaintext)
 
-# Functions from the second code
+
+
 ciphertext_file = 'data/ciphertext.txt'
 iv_file = 'data/iv.txt'
 
-# ----------------------------------------------------------------------------
-# No code executed here; only module imports.
-# ----------------------------------------------------------------------------
 def generate_random_iv():
     return os.urandom(16)
 
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# AES Encryption: Function is used for AES encryption. It takes plaintext, a secret key, and an initialization vector (IV) as input and returns the ciphertext.
 # ----------------------------------------------------------------------------
-def encrypt_aes_cbc(plaintext, key, iv):
+# Function to perform AES encryption in CBC (Cipher Block Chaining) mode
+def encryption_aes(plaintext, key, iv):
+    # Define the block size (AES block size is 16 bytes)
     block_size = 16
+    
+    # Pad the plaintext to match the block size
     plaintext = pad_data(plaintext.encode('utf-8'), block_size)
+    
+    # Initialize an empty ciphertext
     ciphertext = b''
+    
+    # Initialize the previous block with the IV (Initialization Vector)
     prev_block = iv
 
+    # Iterate over the plaintext in blocks
     for i in range(0, len(plaintext), block_size):
-        block = plaintext[i:i + block_size]
+        block = plaintext[i:i + block_size]  # Get the current block
+        
+        # XOR the current block with the previous ciphertext block or IV
         xor_block = bytes(x ^ y for x, y in zip(block, prev_block))
+        
+        # Encrypt the XORed block using the encryption key
         encrypted_block = encrypt_block(xor_block, key)
+        
+        # Append the encrypted block to the ciphertext
         ciphertext += encrypted_block
+        
+        # Update the previous block with the current encrypted block
         prev_block = encrypted_block
 
+    # Return the final ciphertext
     return ciphertext
 
+
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Function to perform AES decryption in CBC (Cipher Block Chaining) mode
 # ----------------------------------------------------------------------------
-def decrypt_aes_cbc(ciphertext, key, iv):
+
+def decrypt_aes(ciphertext, key, iv):
+    # Define the block size (AES block size is 16 bytes)
     block_size = 16
+    
+    # Initialize an empty plaintext
     plaintext = b''
+    
+    # Initialize the previous block with the IV (Initialization Vector)
     prev_block = iv
 
+    # Iterate over the ciphertext in blocks
     for i in range(0, len(ciphertext), block_size):
-        block = ciphertext[i:i + block_size]
+        block = ciphertext[i:i + block_size]  # Get the current block
+        
+        # Decrypt the current block using the decryption key
         decrypted_block = decrypt_block(block, key)
+        
+        # XOR the decrypted block with the previous ciphertext block or IV
         plaintext_block = bytes(x ^ y for x, y in zip(decrypted_block, prev_block))
+        
+        # Append the plaintext block to the overall plaintext
         plaintext += plaintext_block
+        
+        # Update the previous block with the current ciphertext block
         prev_block = block
 
+    # Remove padding and decode the plaintext to UTF-8
     return unpad_data(plaintext).decode('utf-8')
 
+
 # ----------------------------------------------------------------------------
-# No code executed here; only module imports.
+# Function for Decoding
 # ----------------------------------------------------------------------------
+# Function to decrypt ciphertext and write the decrypted result
 def Dec():
+    # Read the encryption key from a file
     sk = read_key_from_file()
-    print("SECRET KEY READ")
     write_key_to_file(sk)
 
+    # Read the ciphertext and IV (Initialization Vector) from files
     c = read_data_from_hex_file(ciphertext_file)
     iv = read_data_from_hex_file(iv_file)
 
-    decrypted_plaintext = decrypt_aes_cbc(c, sk, iv)
+    # Decrypt the ciphertext using the encryption key and IV
+    decrypted_plaintext = decrypt_aes(c, sk, iv)
+
+    # Print the decrypted plaintext to the console
     print("DECRYPTED TEXT: ", decrypted_plaintext)
+
+    # Write the decrypted plaintext to a result file
     write_to_result(decrypted_plaintext)
+
 
 # ----------------------------------------------------------------------------
 # Encode Function: This function is aimed at encoding
 # ----------------------------------------------------------------------------
 def Enc():
     sk = read_key_from_file()
-    print("SECRET KEY READ")
     print_key(sk)
 
     f = read_plain_text()
@@ -204,7 +230,7 @@ def Enc():
     iv = generate_random_iv()
     print("RANDOM IV: ", iv)
 
-    c = encrypt_aes_cbc(f, sk, iv)
+    c = encryption_aes(f, sk, iv)
     print("CIPHERTEXT: ", c)
 
     write_data_to_hex_file(c, ciphertext_file)
